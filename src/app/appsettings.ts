27898740e6bf0e6723169cf5dc32a0cc56ef04c8
Eniga Ahiante <enigaahiante@live.com>
2019-02-19 14:57:20 +0100

    renamed duration to interval for the next on service kinds page

--- src/app/appsettings.ts
+++ src/app/appsettings.ts
@@ -1,5 +1,5 @@
 export class appsettings {
     // public static api_url = "https://localhost:44359/api/"; 
-    public static api_url = "https://lagoshealthreminderapi.azurewebsites.net/api/"; // Test
-    // public static api_url = "http://lagoshealthapi.azurewebsites.net/api/"; // Live
+    // public static api_url = "https://lagoshealthreminderapi.azurewebsites.net/api/"; // Test
+    public static api_url = "https://lagoshealthapi.azurewebsites.net/api/"; // Live
 }
